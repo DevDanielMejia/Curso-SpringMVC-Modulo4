c57c3d436cbc5857887a9189648e637056a0248c
DevDanielMejia <73041626+DevDanielMejia@users.noreply.github.com>
2025-04-28 20:57:27 -0500

    Modulo 4 finalizado

--- src/main/java/com/daniel/curso/springboot/di/factura/springboot_difactura/AppConfig.java
+++ src/main/java/com/daniel/curso/springboot/di/factura/springboot_difactura/AppConfig.java
@@ -14,10 +14,20 @@ import com.daniel.curso.springboot.di.factura.springboot_difactura.models.Produc
 @PropertySource(value = "classpath:data.properties", encoding = "UTF-8")
 public class AppConfig {
 
-    @Bean
+    @Bean("default")
     List<Item> itemsInvoice(){
         Product p1 = new Product("Camara Sóny", 800);
         Product p2 = new Product("Bicicleta", 1200);
         return Arrays.asList(new Item(p1,2), new Item(p2,4));
+    }
+
+    @Bean
+    //@Primary
+    List<Item> itemsInvoiceOffice(){
+        Product p1 = new Product("Monitor Asus", 700);
+        Product p2 = new Product("Notebook", 2400);
+        Product p3 = new Product("Impresora", 800);
+        Product p4 = new Product("Escritorio", 900);
+        return Arrays.asList(new Item(p1,4), new Item(p2,6), new Item(p3,1), new Item(p4,4));
     }
 }
--- src/main/java/com/daniel/curso/springboot/di/factura/springboot_difactura/models/Invoice.java
+++ src/main/java/com/daniel/curso/springboot/di/factura/springboot_difactura/models/Invoice.java
@@ -3,20 +3,48 @@ package com.daniel.curso.springboot.di.factura.springboot_difactura.models;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.context.annotation.RequestScope;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
 @Component
+@RequestScope
+//@JsonIgnoreProperties({"targetSource","advisors"})
 public class Invoice {
 
     @Autowired
     private Client client;
 
-    @Value("${invoice.description}")
+    @Value("${invoice.description.office}")
     private String description;
 
     @Autowired
+    @Qualifier("default")
     private List<Item> items;
+
+    public Invoice(){
+        System.out.println("Creando el componende de la factura 1");
+        
+    }
+
+    @PostConstruct
+    public void init(){  
+        System.out.println("Creando el componende de la factura 2");  
+         client.setName(client.getName().concat(" Pepe"));
+         description = description.concat(" del cliente: ").concat(client.getName().concat(" ").concat(client.getLastname()));
+    }
+
+    @PreDestroy
+    public void destroy(){
+        System.out.println("Destruyendo componente Invoice");
+    }
 
     public Client getClient() {
         return client;
@@ -35,6 +63,17 @@ public class Invoice {
     }
     public void setItems(List<Item> items) {
         this.items = items;
+    }
+
+    public int getTotal(){
+        // int total = 0;
+        // for (Item item : items) {
+        //     total += item.getImport();
+        // }
+        int total = items.stream()
+        .map(item -> item.getImport())
+        .reduce(0,(sum, importe) -> sum + importe);
+        return total;
     }
 
     
